6ec6b46ff0be1556da8c5f711e5dcf858b1fccf2
berheruben <rubenberhe56@gmail.com>
2021-11-15 12:28:04 +0100

    commit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,9 +9,6 @@ import { Data } from '@angular/router';
 export class DataService {
   configUrl = 'assets/config.json';
 
-  getConfig() {
-    return this.http.get<data>(this.configUrl);
-  }
   
   constructor(private http: HttpClient) { 
     
